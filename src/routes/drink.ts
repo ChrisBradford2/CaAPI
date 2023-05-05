416844b1262110fadeb2b11dcf8e4e1edfcc927d
Nicolas Barbarisi <nico.bar2012@gmail.com>
2023-05-05 18:08:57 +0200

    Security : Change priority of drink route security.

--- src/routes/drink.ts
+++ src/routes/drink.ts
@@ -4,10 +4,10 @@ import limiter from "../utils/ratelimit";
 const auth = require('../middleware/auth');
 const drinkCtrl = require('../controllers/drink');
 
-router.post('/', auth, limiter, drinkCtrl.createDrink);
-router.put('/:id', auth, limiter, drinkCtrl.modifyDrink);
-router.delete('/:id', auth, limiter, drinkCtrl.deleteDrink);
-router.get('/', auth, limiter, drinkCtrl.getAllDrink);
-router.get('/:id', auth, limiter, drinkCtrl.getOneDrink);
+router.post('/',limiter, auth, drinkCtrl.createDrink);
+router.put('/:id',limiter, auth, drinkCtrl.modifyDrink);
+router.delete('/:id',limiter, auth, drinkCtrl.deleteDrink);
+router.get('/',limiter, auth, drinkCtrl.getAllDrink);
+router.get('/:id',limiter, auth, drinkCtrl.getOneDrink);
 
 module.exports = router;
