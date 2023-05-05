5137d33ad3c2e350e38b5d6ca42ccd3ae8312f83
Nicolas Barbarisi <nico.bar2012@gmail.com>
2023-05-05 17:43:52 +0200

    Feature : Add jsdoc.

--- src/models/Drink.ts
+++ src/models/Drink.ts
@@ -1,5 +1,15 @@
 import mongoose from 'mongoose';
 
+/**
+ * Drink model
+ *
+ * @typedef {Object} Drink
+ *
+ * @property {String} name.required - Name of the drink
+ * @property {Number} caffein_level.required - Caffein level of the drink
+ * @property {Number} volume.required - Volume of the drink
+ */
+
 const drinkSchema = new mongoose.Schema({
   name: { type: String, required: true },
   caffein_level: { type: Number, required: true },
