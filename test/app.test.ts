68c4242a01927ab82613577e2df3d503e5821b5d
Nicolas Barbarisi <nico.bar2012@gmail.com>
2023-05-09 16:58:59 +0200

    Fix : Test is up

--- test/app.test.ts
+++ test/app.test.ts
@@ -3,17 +3,3 @@ describe('Test', () => {
     expect(true).toBe(true);
   });
 });
-
-/*
-describe('Drink', () => {
-  it('should get all drinks from database', async () => {
-    const drinks = await Drink.find();
-    expect(drinks).toBeDefined();
-  });
-  it('should get one drink from database', async () => {
-    const drink = await Drink.findOne({ _id: '644c26ea111203cf91b37c44' });
-    expect(drink).toBeDefined();
-  });
-});
-
-*/
